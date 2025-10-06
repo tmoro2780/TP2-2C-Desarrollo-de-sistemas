d1238f65a3c05baa4c43f4e6a223923db8db18bd
Tomas Muruchi <212407299+tmoro2780@users.noreply.github.com>
2025-10-06 02:09:48 -0300

    ....

---
+++ backend/src/services/userService.ts
@@ -0,0 +1,10 @@
+// En este modulo se crean las querys que se van a realizar 
+// a la base de datoa usando Prisma como ORM
+import { db } from "../config/db";
+
+//Esta clase se encarga de realizar las operaciones CRUD
+// sobre la tabla usuarios en la base de datos.
+export class UserService {
+
+
+}
