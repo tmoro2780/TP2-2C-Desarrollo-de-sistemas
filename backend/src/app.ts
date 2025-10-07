82e64deef1ffb4e43004eea8aa1d7aa00ed7c605
tmoro2780 <tmuruchi27800@gmail.com>
2025-10-07 00:40:26 +0000

    Configuracion CORS para permitir solicitudes desde el frontend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,17 @@
 import express, { Request, Response } from 'express';
+import cors from "cors";
+
+
 
 import { event_router } from './routers/eventRouter';
 
 export const app = express();
+
+// Configuración de CORS Permite las solicitudes desde el frontend
+app.use(cors({
+  origin: 'http://localhost:5173', // ← frontend
+  credentials: true, // si usas cookies o tokens con credenciales
+}));
 
 app.use(express.json());
 app.use('/eventos', event_router);
